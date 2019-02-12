e77d6ae9f27ae2ae3ceede3ef6447a35a7b8991e
CriTicalSSX <sam.glendenning@btinternet.com>
2019-02-12 15:38:00 +0000

    Speed works with preset values

---
+++ Assets/bounce.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class bounce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(new Vector2(1, 1) * 1000f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.Find("player");
+        if (player.transform.position.y <= -12)
+        {
+            GameObject background = GameObject.Find("background");
+            scroll scroll = background.GetComponent<scroll>();
+            changeSpeed(scroll.speed);
+        }
+    }
+
+    void changeSpeed(float speed)
+    {
+        GameObject background = GameObject.Find("background");
+        Renderer renderer = background.GetComponent(typeof(Renderer)) as Renderer;
+        Vector2 offset = new Vector2(Time.time * speed, 0);
+        renderer.material.mainTextureOffset = offset;
+    }
+}
---
+++ Assets/scroll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour {
+
+    public float speed;
+    Vector2 offset;
+
+    // Use this for initialization
+    void Start () {
+        speed = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed -= 0.002f;
+
+        if (speed >= 0)
+        {
+            offset = new Vector2(Time.time * speed, 0);
+
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTextureOffset = offset;
+        }
+        else
+        {
+            speed = 0;
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.SetTextureOffset("new", renderer.material.GetTextureOffset("background"));
+        }
+	}
+}
